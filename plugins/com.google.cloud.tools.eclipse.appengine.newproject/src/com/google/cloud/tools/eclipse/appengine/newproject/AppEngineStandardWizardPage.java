9748a1fa37356b4e4ecc454efc395e6aeb87c409
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2016-05-16 11:30:52 -0400

    add an icon to the wizard page (#158)
    * add an icon to the wizard page

--- plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/AppEngineStandardWizardPage.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/AppEngineStandardWizardPage.java
@@ -1,7 +1,9 @@
 package com.google.cloud.tools.eclipse.appengine.newproject;
 
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -19,9 +21,17 @@ class AppEngineStandardWizardPage extends WizardNewProjectCreationPage implement
   private Text projectIdField;
   
   AppEngineStandardWizardPage() {
-    super("basicNewProjectPage");
+    super("basicNewProjectPage"); //$NON-NLS-1$
+    // todo instead of hard coding strings, read the wizard.name and wizard.description properties 
+    // from plugins/com.google.cloud.tools.eclipse.appengine.newproject/plugin.properties
     this.setTitle("App Engine Standard Project");
-    this.setDescription("Create a new App Engine Standard Project in the workspace.");
+    this.setDescription("Create a new App Engine Standard Project in the workspace."); 
+    
+    // todo get a UI designer to pick a better icon (the little plane?) and 
+    // add it to this plugin's icons folder
+    ImageDescriptor descriptor = AbstractUIPlugin.imageDescriptorFromPlugin(
+        "com.google.cloud.tools.eclipse.appengine.localserver", "icons/gcp-32x32.png"); //$NON-NLS-1$ //$NON-NLS-2$
+    this.setImageDescriptor(descriptor);
   }
 
   // todo is there a way to call this for a test?
@@ -34,7 +44,7 @@ class AppEngineStandardWizardPage extends WizardNewProjectCreationPage implement
     
     // Java package name
     Label packageNameLabel = new Label(container, SWT.NONE);
-    packageNameLabel.setText("Java package:");
+    packageNameLabel.setText("Java package:"); //$NON-NLS-1$
     javaPackageField = new Text(container, SWT.BORDER);
     GridData javaPackagePosition = new GridData(GridData.FILL_HORIZONTAL);
     javaPackagePosition.horizontalSpan = 2;
@@ -43,7 +53,7 @@ class AppEngineStandardWizardPage extends WizardNewProjectCreationPage implement
     
     // App Engine Project ID
     Label projectIdLabel = new Label(container, SWT.NONE);
-    projectIdLabel.setText("App Engine Project ID: (optional)");
+    projectIdLabel.setText("App Engine Project ID: (optional)"); //$NON-NLS-1$
     projectIdField = new Text(container, SWT.BORDER);
     GridData projectIdPosition = new GridData(GridData.FILL_HORIZONTAL);
     projectIdPosition.horizontalSpan = 2;
@@ -65,13 +75,13 @@ class AppEngineStandardWizardPage extends WizardNewProjectCreationPage implement
   private boolean validateLocalFields() {
     String packageName = javaPackageField.getText();
     if (!JavaPackageValidator.validate(packageName)) {
-      setErrorMessage("Illegal Java package name: " + packageName);
+      setErrorMessage("Illegal Java package name: " + packageName); //$NON-NLS-1$
       return false;
     }
     
     String projectId = projectIdField.getText();
     if (!AppEngineProjectIdValidator.validate(projectId)) {
-      setErrorMessage("Illegal App Engine Project ID: " + projectId);
+      setErrorMessage("Illegal App Engine Project ID: " + projectId); //$NON-NLS-1$
       return false;
     }
     
