0cdffd7f9ffb698bee3175cbd90161ba7f2e3dc0
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2016-05-17 14:14:23 -0400

    better checking of package names (#174)
    * better checking of package names
    * use JavaConventions from Eclipse JDT to check package names

--- plugins/com.google.cloud.tools.eclipse.appengine.newproject.test/src/com/google/cloud/tools/eclipse/appengine/newproject/JavaPackageValidatorTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.newproject.test/src/com/google/cloud/tools/eclipse/appengine/newproject/JavaPackageValidatorTest.java
@@ -14,6 +14,11 @@ public class JavaPackageValidatorTest {
   public void testEndsWithPeriod() {
     Assert.assertFalse(JavaPackageValidator.validate("com.google.eclipse."));
   }
+  
+  @Test
+  public void testStartsWithPeriod() {
+    Assert.assertFalse(JavaPackageValidator.validate(".com.google.eclipse."));
+  }
 
   @Test
   public void testOneWord() {
@@ -22,10 +27,26 @@ public class JavaPackageValidatorTest {
   }
   
   @Test
-  public void testContainsSpace() {
+  public void testContainsSpaceAroundPeriod() {
     Assert.assertFalse(JavaPackageValidator.validate("com. google.eclipse"));
   }
+  
+  @Test
+  public void testContainsInternalSpace() {
+    Assert.assertFalse(JavaPackageValidator.validate("com.goo gle.eclipse"));
+  }
+  
+  @Test
+  public void testDoublePeriod() {
+    Assert.assertFalse(JavaPackageValidator.validate("com..google"));
+  }
 
+
+  @Test
+  public void testJavaKeyword() {
+    Assert.assertFalse(JavaPackageValidator.validate("com.new"));
+  }
+  
   @Test
   public void testEmptyString() {
     Assert.assertTrue(JavaPackageValidator.validate(""));
--- plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/JavaPackageValidator.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/JavaPackageValidator.java
@@ -1,10 +1,14 @@
 package com.google.cloud.tools.eclipse.appengine.newproject;
 
+import org.eclipse.jdt.core.JavaConventions;
+import org.eclipse.jdt.core.JavaCore;
+
 public class JavaPackageValidator {
 
   /**
    * Check if a string is a legal Java package name.
    */
+  // todo return an IStatus for better error reporting
   public static boolean validate(String packageName) {
     if (packageName == null) {
       return false;
@@ -13,27 +17,27 @@ public class JavaPackageValidator {
     } else if (packageName.endsWith(".")) {
       // todo or allow this and strip the period
       return false;
-    } else {
-      String[] parts = packageName.split("\\.");
-      for (int i = 0; i < parts.length; i++) {
-        if (!isValidJavaName(parts[i])) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
-  private static boolean isValidJavaName(String name) {
-    if (!Character.isJavaIdentifierStart(name.charAt(0))) {
+    } else if (containsWhitespace(packageName)) {
+      // note very weird condition because validatePackageName allows internal white space
       return false;
+    } else {
+      return JavaConventions.validatePackageName(
+          packageName, JavaCore.VERSION_1_4, JavaCore.VERSION_1_4).isOK();
     }
-    for (int i = 1; i < name.length(); i++) {
-      if (!Character.isJavaIdentifierPart(name.charAt(i))) {
-        return false;
+  }
+  
+  /**
+   * Checks whether this string contains any C0 controls (characters with code 
+   * point <= 0x20). This is the definition of white space used by String.trim()
+   * which is what we're prechecking here.
+   */
+  private static boolean containsWhitespace(String s) {
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) <= 0x20) {
+        return true;
       }
     }
-    return true;
+    return false;
   }
 
 }
