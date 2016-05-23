7c2ed326d89d92f0648fe6a6084de538b0d4c8b1
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2016-05-23 17:04:25 -0400

    locate cloud SDK (#191)

--- plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/CreateAppEngineStandardWtpProject.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/CreateAppEngineStandardWtpProject.java
@@ -3,8 +3,10 @@ package com.google.cloud.tools.eclipse.appengine.newproject;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.commands.ExecutionException;
@@ -38,6 +40,7 @@ import org.eclipse.wst.common.project.facet.core.runtime.RuntimeManager;
 import org.eclipse.wst.server.core.IRuntimeType;
 import org.eclipse.wst.server.core.IRuntimeWorkingCopy;
 import org.eclipse.wst.server.core.ServerCore;
+import org.eclipse.wst.server.core.internal.RuntimeWorkingCopy;
 
 /**
 * Utility to make a new Eclipse project with the App Engine Standard facets in the workspace.  
@@ -118,24 +121,38 @@ class CreateAppEngineStandardWtpProject extends WorkspaceModifyOperation {
     if (RuntimeManager.isRuntimeDefined("App Engine")) {
       IRuntime appEngineRuntime = RuntimeManager.getRuntime("App Engine");
       project.setPrimaryRuntime(appEngineRuntime, monitor);
-    } else {
-      // Create a new App Engine runtime
+    } else { // Create a new App Engine runtime
       IRuntimeType appEngineRuntimeType =
-        ServerCore.findRuntimeType("com.google.cloud.tools.eclipse.gcloud.runtime");
+          ServerCore.findRuntimeType("com.google.cloud.tools.eclipse.gcloud.runtime");
       if (appEngineRuntimeType == null) {
-        throw new NullPointerException("Could not locate App Engine runtime");
+        throw new NullPointerException("Could not find App Engine runtime type");
       }
 
-	  IRuntimeWorkingCopy appEngineRuntimeWC = appEngineRuntimeType.createRuntime(null, monitor);
-	  org.eclipse.wst.server.core.IRuntime appEngineServerRuntime = appEngineRuntimeWC.save(true, monitor);
-	  IRuntime appEngineFacetRuntime = FacetUtil.getRuntime(appEngineServerRuntime);
-	  if (appEngineFacetRuntime == null) {
-	    throw new NullPointerException("Null App Engine facet runtime");
-	  }
-
-	  project.addTargetedRuntime(appEngineFacetRuntime, monitor);
-	  project.setPrimaryRuntime(appEngineFacetRuntime, monitor);
+      IRuntimeWorkingCopy appEngineRuntimeWorkingCopy 
+          = appEngineRuntimeType.createRuntime(null, monitor);
+      
+      // Need to use internal API to access Generic Server Framework properties
+      RuntimeWorkingCopy mutator = (RuntimeWorkingCopy) appEngineRuntimeWorkingCopy;
+      Map<String, String> map = mutator.getAttribute(
+          "generic_server_instance_properties", new HashMap<>());
+      map.put("cloudSdkDirectory", findCloudSdk());
+      mutator.setAttribute("generic_server_instance_properties", map);
+      
+      org.eclipse.wst.server.core.IRuntime appEngineServerRuntime 
+          = appEngineRuntimeWorkingCopy.save(true, monitor);
+      IRuntime appEngineFacetRuntime = FacetUtil.getRuntime(appEngineServerRuntime);
+      if (appEngineFacetRuntime == null) {
+        throw new NullPointerException("Could not locate App Engine facet runtime");
+      }
+  
+      project.addTargetedRuntime(appEngineFacetRuntime, monitor);
+      project.setPrimaryRuntime(appEngineFacetRuntime, monitor);
     }
+  }
+
+  private static String findCloudSdk() {
+    // todo: replace with PathResolver from app-tools-lib-for-java
+    return "/usr/local/google/google-cloud-sdk";
   }
 
 }
