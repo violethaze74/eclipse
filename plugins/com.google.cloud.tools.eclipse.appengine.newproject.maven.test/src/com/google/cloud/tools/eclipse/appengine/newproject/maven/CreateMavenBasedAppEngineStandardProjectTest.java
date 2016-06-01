7e16556b522672329675660dae43898ec0e0a519
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2016-06-01 11:16:14 -0700

    remove unused code (#208)

--- plugins/com.google.cloud.tools.eclipse.appengine.newproject.maven.test/src/com/google/cloud/tools/eclipse/appengine/newproject/maven/CreateMavenBasedAppEngineStandardProjectTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.newproject.maven.test/src/com/google/cloud/tools/eclipse/appengine/newproject/maven/CreateMavenBasedAppEngineStandardProjectTest.java
@@ -1,12 +1,8 @@
 package com.google.cloud.tools.eclipse.appengine.newproject.maven;
 
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.m2e.core.project.IProjectConfigurationManager;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -21,15 +17,6 @@ public class CreateMavenBasedAppEngineStandardProjectTest {
   private IProjectConfigurationManager manager;
 
   private NullProgressMonitor monitor = new NullProgressMonitor();
-  
-  @Before
-  public void setUp() throws CoreException {
-    IWorkspace workspace = ResourcesPlugin.getWorkspace();
-  }
-  
-  @After
-  public void cleanUp() throws CoreException {
-  }
   
   @Test
   public void testConstructor()
