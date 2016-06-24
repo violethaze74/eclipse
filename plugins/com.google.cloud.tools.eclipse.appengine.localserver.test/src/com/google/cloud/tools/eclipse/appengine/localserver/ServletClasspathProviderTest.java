42f719a8a3638e9ab1ed786fc43e940ba63aa00e
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2016-06-24 19:28:31 -0400

    install google cloud sdk on travis (#292)
    * install google cloud sdk on travis
    * remove travis hack

--- plugins/com.google.cloud.tools.eclipse.appengine.localserver.test/src/com/google/cloud/tools/eclipse/appengine/localserver/ServletClasspathProviderTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.localserver.test/src/com/google/cloud/tools/eclipse/appengine/localserver/ServletClasspathProviderTest.java
@@ -12,15 +12,8 @@ public class ServletClasspathProviderTest {
   @Test
   public void testResolveClasspathContainer() {
      IClasspathEntry[] result = provider.resolveClasspathContainer(null, null);
-     if (result.length == 2) {
-       Assert.assertTrue(result[0].getPath().toString().endsWith("servlet-api.jar"));
-       Assert.assertTrue(result[1].getPath().toString().endsWith("jsp-api.jar"));
-     } else {
-       // hack because gcloud is not yet installed on Travis.
-       // see https://github.com/GoogleCloudPlatform/gcloud-eclipse-tools/issues/267
-       // TODO fix this!
-       Assert.assertEquals(0, result.length);
-     }
+     Assert.assertTrue(result[0].getPath().toString().endsWith("servlet-api.jar"));
+     Assert.assertTrue(result[1].getPath().toString().endsWith("jsp-api.jar"));
   }
 
 }
