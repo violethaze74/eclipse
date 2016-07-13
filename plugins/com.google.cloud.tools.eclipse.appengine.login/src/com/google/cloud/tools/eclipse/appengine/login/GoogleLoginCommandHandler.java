39f2c0a94a01e6bd9f0314ecc55cc31ad04b5c25
Chanseok Oh <chanseok@google.com>
2016-07-13 16:47:36 -0400

    Add no-op login menu (#343)
    * Add no-op login menu
    * Localize menu strings
    * Move menu item location / change tooltip
    * Move toolbar position / move menu item to "File"
    * Move login button to bottom / use 'G' shortcut key
    * Include icons in binary

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.login/src/com/google/cloud/tools/eclipse/appengine/login/GoogleLoginCommandHandler.java
@@ -0,0 +1,18 @@
+package com.google.cloud.tools.eclipse.appengine.login;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+public class GoogleLoginCommandHandler extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(window.getShell(),
+		    "GCloud Login Support", "Login: to be supported");
+		return null;
+	}
+}
