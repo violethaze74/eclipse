07e8f22cf7284c3578cd24ffdc556fa1657bb7b6
nbashirbello <nbashirbello@google.com>
2016-07-13 09:41:53 -0400

    Ensure that app-tools-lib-for-java runtime can be added to faceted projects (#352)
    * Rename the GAE custom runtime component type as it currently has the same name as the GAE runtime type
    
    * Added a runtime facet mapping to map the existing runtime component to the app tools lib runtime also;
    
    * Update the AppToolsRuntime validate method.

--- plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/server/AppToolsRuntime.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/server/AppToolsRuntime.java
@@ -1,7 +1,14 @@
 package com.google.cloud.tools.eclipse.appengine.localserver.server;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.wst.server.core.model.RuntimeDelegate;
 
 public class AppToolsRuntime extends RuntimeDelegate {
+  @Override
+  public IStatus validate() {
+    // TODO Check validation from app tools lib?
+    return Status.OK_STATUS;
+  }
 
 }
