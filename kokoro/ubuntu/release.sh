9518fc5ab173a2017f2c0c4211f09d656936fd01
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2016-09-12 11:37:57 -0400

    release scripts for kokoro (#659)
    * release configs
    * create a timestamp file
    * release tags

---
+++ kokoro/ubuntu/release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+# Display commands being run.
+set -x
+
+# create a timestamp file
+date +%s > timestamp.txt
+
+cd git/google-cloud-eclipse
+./build.sh
+
+
