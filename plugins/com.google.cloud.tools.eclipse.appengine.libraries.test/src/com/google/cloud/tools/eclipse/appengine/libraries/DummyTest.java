75aa596cb560f43315e8495b65e96aa617d170e5
Andras Kerekes <akerekes@users.noreply.github.com>
2016-09-27 11:28:47 -0400

    Fixes #733 - New bundle for API management (#734)
    * Fixes #733 - New bundle for API management
    
    * renamed bundles
    
    * remove empty comment block from .project

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.libraries.test/src/com/google/cloud/tools/eclipse/appengine/libraries/DummyTest.java
@@ -0,0 +1,11 @@
+package com.google.cloud.tools.eclipse.appengine.libraries;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+  @Test
+  public void test() {
+    // make surefire happy
+  }
+}
