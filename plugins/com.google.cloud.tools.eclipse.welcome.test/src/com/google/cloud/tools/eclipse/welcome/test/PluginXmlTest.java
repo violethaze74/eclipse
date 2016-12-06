3ac62976fa142dc79b13ebad3210d26ff977b27f
Chanseok Oh <chanseok@google.com>
2016-12-06 10:15:50 -0500

    Add main Wiki link to the Overview section in the Welcome intro page (#1052)
    * Add CT4E landing page link to Welcome > Overview
    * Update project configs
    * Put .welcome module in pom.xml
    * Add .welcome.test bundle and PluginXmlTest
    * Add a GCP icon to the link

---
+++ plugins/com.google.cloud.tools.eclipse.welcome.test/src/com/google/cloud/tools/eclipse/welcome/test/PluginXmlTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.eclipse.welcome.test;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.cloud.tools.eclipse.test.util.BasePluginXmlTest;
+import org.junit.Test;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class PluginXmlTest extends BasePluginXmlTest {
+
+  @Test
+  public void testExtensionPoint() {
+    NodeList extensions = getDoc().getElementsByTagName("extension");
+    assertEquals(1, extensions.getLength());
+    Element extension = (Element) extensions.item(0);
+    assertEquals("org.eclipse.ui.intro.configExtension", extension.getAttribute("point"));
+
+    NodeList configExtensions = extension.getElementsByTagName("configExtension");
+    assertEquals(1, configExtensions.getLength());
+    Element configExtension = (Element) configExtensions.item(0);
+    assertEquals("intro/cloud-tools-for-eclipse.xml", configExtension.getAttribute("content"));
+    assertEquals("org.eclipse.ui.intro.universalConfig", configExtension.getAttribute("configId"));
+  }
+}
