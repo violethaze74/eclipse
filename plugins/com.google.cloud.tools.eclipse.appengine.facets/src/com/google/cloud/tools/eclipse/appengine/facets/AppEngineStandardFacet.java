9e475d8d275a353b814145ec9c5e771c44a54eb1
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2017-03-01 11:50:02 -0500

    update default app engine Java SDK (#1505)

--- plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/AppEngineStandardFacet.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/AppEngineStandardFacet.java
@@ -67,7 +67,7 @@ public class AppEngineStandardFacet {
   static final String DEFAULT_RUNTIME_ID =
       "com.google.cloud.tools.eclipse.appengine.standard.runtime";
   static final String DEFAULT_RUNTIME_NAME = "App Engine Standard";
-  public static final String DEFAULT_APPENGINE_SDK_VERSION = "1.9.46";
+  public static final String DEFAULT_APPENGINE_SDK_VERSION = "1.9.49";
   public static final String DEFAULT_GCLOUD_PLUGIN_VERSION = "2.0.9.133.v201611104";
 
   /**
