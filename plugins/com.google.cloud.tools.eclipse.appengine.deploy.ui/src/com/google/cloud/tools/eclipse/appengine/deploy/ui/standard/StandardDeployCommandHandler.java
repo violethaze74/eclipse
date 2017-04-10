60aa099c66ccbbef7d7ad54aa12840af2b88286c
Chanseok Oh <chanseok@google.com>
2017-04-10 10:50:22 -0400

    Send Analytics ping only after deploy succeeds (#1763)

--- plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/standard/StandardDeployCommandHandler.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/standard/StandardDeployCommandHandler.java
@@ -110,7 +110,6 @@ public class StandardDeployCommandHandler extends AbstractHandler {
 
   private static void launchDeployJob(IProject project, Credential credential)
                                                             throws IOException, ExecutionException {
-
     AnalyticsPingManager.getInstance().sendPing(
         AnalyticsEvents.APP_ENGINE_DEPLOY, AnalyticsEvents.APP_ENGINE_DEPLOY_STANDARD, null);
 
@@ -136,8 +135,10 @@ public class StandardDeployCommandHandler extends AbstractHandler {
 
       @Override
       public void done(IJobChangeEvent event) {
-        AnalyticsPingManager.getInstance().sendPing(AnalyticsEvents.APP_ENGINE_DEPLOY_SUCCESS,
-            AnalyticsEvents.APP_ENGINE_DEPLOY_STANDARD, null);
+        if (event.getResult().isOK()) {
+          AnalyticsPingManager.getInstance().sendPing(AnalyticsEvents.APP_ENGINE_DEPLOY_SUCCESS,
+              AnalyticsEvents.APP_ENGINE_DEPLOY_STANDARD, null);
+        }
         launchCleanupJob();
       }
     });
