0a0def7543859e77c5c44024e26c8b62b0a0e013
Brian de Alwis <bsd@mt.ca>
2017-04-28 12:31:44 -0400

    Fix some remaining build nits (#1878)
    - fix up and add missing config files in .appengine.standard.java8* bundles
      and make robust to Maven > Update Projects
    - update jacoco build dependencies
      - automate more in gendependencies.sh script
      - fix up whitespace in .datacore.ui* poms

--- build/jacoco/gendependencies.sh
+++ build/jacoco/gendependencies.sh
@@ -3,27 +3,30 @@
 # This script will need updating if we go pomless.
 
 xml sel -I -N 'mvn=http://maven.apache.org/POM/4.0.0' -t \
-  --elem dependency \
-    --elem groupId \
-        --if '/mvn:project/mvn:groupId' \
-            -v '/mvn:project/mvn:groupId' \
-        --else \
-            -v '/mvn:project/mvn:parent/mvn:groupId' \
-        --break \
-    --break \
-    --elem artifactId \
-        -v '/mvn:project/mvn:artifactId' \
-        --break \
-    --elem version \
-        -v '/mvn:project/mvn:version' \
-        --break \
-    --elem scope \
-        --if '/mvn:project/mvn:packaging = "eclipse-plugin"' \
-          -o 'compile' \
-        --elif '/mvn:project/mvn:packaging = "eclipse-test-plugin"' \
-          -o 'test' \
-        --else \
-          -o 'unknown' \
-        --break \
-        --break \
-../../plugins/*/pom.xml
+  --if 'not(/mvn:project/mvn:artifactId = "com.google.cloud.tools.eclipse.test.dependencies")' \
+    --elem dependency \
+      --elem groupId \
+          --if '/mvn:project/mvn:groupId' \
+              -v '/mvn:project/mvn:groupId' \
+          --else \
+              -v '/mvn:project/mvn:parent/mvn:groupId' \
+          --break \
+      --break \
+      --elem artifactId \
+          -v '/mvn:project/mvn:artifactId' \
+          --break \
+      --elem version \
+          -v '/mvn:project/mvn:version' \
+          --break \
+      --elem scope \
+          --if '/mvn:project/mvn:packaging = "eclipse-plugin"' \
+            -o 'compile' \
+          --elif '/mvn:project/mvn:packaging = "eclipse-test-plugin"' \
+            -o 'test' \
+          --else \
+            -o 'unknown' \
+          --break \
+          --break \
+  --break \
+../../plugins/*/pom.xml \
+| sed 's;dependency xmlns="http://maven.apache.org/POM/4.0.0";dependency;'
