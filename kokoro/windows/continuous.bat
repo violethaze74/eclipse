b92708d5e7b8e25d0814c4693ffffaf97aceb1a5
Chanseok Oh <chanseok@google.com>
2017-05-05 10:57:42 -0400

    Use "gcloud" and "mvn" provided in Kokoro Windows env / suppress "mvn" garbage log messages (#1907)
    * Suppress garbage log messages
    * Use built-in gcloud and mvn
    * Set up and use 3.5.0 Maven wrapper

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -1,16 +1,10 @@
 cd github/google-cloud-eclipse
 
 set CLOUDSDK_CORE_DISABLE_USAGE_REPORTING=true
-wget https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe
-start /WAIT GoogleCloudSDKInstaller.exe /S /noreporting /nostartmenu /nodesktop /logtofile /D=T:\google
-call t:\google\google-cloud-sdk\bin\gcloud.cmd components copy-bundled-python>>python_path.txt && SET /p CLOUDSDK_PYTHON=<python_path.txt && DEL python_path.txt
-call t:\google\google-cloud-sdk\bin\gcloud.cmd components update --quiet
-call t:\google\google-cloud-sdk\bin\gcloud.cmd components install app-engine-java --quiet
-set GOOGLE_CLOUD_SDK_HOME=t:\google\google-cloud-sdk
+call gcloud.cmd components update --quiet
+call gcloud.cmd components install app-engine-java --quiet
 
-wget http://www-us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.zip
-unzip apache-maven-3.5.0-bin.zip
-
-apache-maven-3.5.0/bin/mvn --fail-at-end -Ptravis -Declipse.target=oxygen verify
+mvn -B -N io.takari:maven:wrapper -Dmaven=3.5.0
+mvnw.cmd -B --fail-at-end -Ptravis -Declipse.target=oxygen verify
 
 exit /b %ERRORLEVEL%
