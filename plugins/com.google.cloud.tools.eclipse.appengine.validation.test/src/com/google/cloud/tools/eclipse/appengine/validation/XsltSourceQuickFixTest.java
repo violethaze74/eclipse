520f4dded7f4f721fed7e63444622f3b60395513
Brian de Alwis <bsd@mt.ca>
2017-07-21 09:54:34 -0400

    Fix XsltSourceQuickFixTest.testApply() (#2186)
    - Spin the SWT event loop to process any events
    - Use Hamcrest containsString() to show current contents

--- plugins/com.google.cloud.tools.eclipse.appengine.validation.test/src/com/google/cloud/tools/eclipse/appengine/validation/XsltSourceQuickFixTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.validation.test/src/com/google/cloud/tools/eclipse/appengine/validation/XsltSourceQuickFixTest.java
@@ -16,10 +16,10 @@
 
 package com.google.cloud.tools.eclipse.appengine.validation;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
 
 import com.google.cloud.tools.eclipse.appengine.facets.AppEngineStandardFacet;
 import com.google.cloud.tools.eclipse.test.util.ArrayAssertions;
@@ -68,9 +68,12 @@ public class XsltSourceQuickFixTest {
     IWorkbench workbench = PlatformUI.getWorkbench();
     IEditorPart editorPart = WorkbenchUtil.openInEditor(workbench, file);
     ITextViewer viewer = ValidationTestUtils.getViewer(file);
-    String preContents = viewer.getDocument().get();
+    while(workbench.getDisplay().readAndDispatch()) {
+      // spin the event loop
+    }
 
-    assertTrue(preContents.contains("application"));
+    String preContents = viewer.getDocument().get();
+    assertThat(preContents, containsString("application"));
 
     ProjectUtils.waitForProjects(project);
     assertEquals(1, file.findMarkers(BLACKLIST_MARKER, true, IResource.DEPTH_ZERO).length);
@@ -81,8 +84,8 @@ public class XsltSourceQuickFixTest {
 
     IDocument document = viewer.getDocument();
     String contents = document.get();
-    assertFalse(contents.contains("application"));
-    assertFalse(contents.contains("?><appengine"));
+    assertThat(contents, not(containsString("application")));
+    assertThat(contents, not(containsString("?><appengine")));
 
     // https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/1527
     editorPart.doSave(new NullProgressMonitor());
