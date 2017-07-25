1cc34d2cd14910a391b17ecf47ae28cb8f032a2f
Brian de Alwis <bsd@mt.ca>
2017-07-25 13:09:33 -0400

    Add facet presets for AESv7, v8, and AE Flexible (war) (#2189)

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.facets.test/src/com/google/cloud/tools/eclipse/appengine/facets/AppEngineFacetPresetsTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.eclipse.appengine.facets;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import org.eclipse.jst.common.project.facet.core.JavaFacet;
+import org.eclipse.jst.j2ee.web.project.facet.WebFacetUtils;
+import org.eclipse.wst.common.project.facet.core.IPreset;
+import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
+import org.junit.Test;
+
+/**
+ * Test the App Engine facet presets.
+ */
+public class AppEngineFacetPresetsTest {
+  @Test
+  public void appEngineStandardJava7presetExists() {
+    IPreset preset = ProjectFacetsManager
+        .getPreset("com.google.cloud.tools.eclipse.appengine.standard.jre7.preset");
+    assertNotNull(preset);
+    assertEquals("App Engine standard environment with Java 7, Servlet 2.5", preset.getLabel());
+    assertThat(preset.getProjectFacets(), hasItem(JavaFacet.VERSION_1_7));
+    assertThat(preset.getProjectFacets(), hasItem(WebFacetUtils.WEB_25));
+    assertThat(preset.getProjectFacets(), hasItem(AppEngineStandardFacet.JRE7));
+  }
+
+  @Test
+  public void appEngineFlexiblePresetExists() {
+    IPreset preset = ProjectFacetsManager
+        .getPreset("com.google.cloud.tools.eclipse.appengine.flexible.war.preset");
+    assertNotNull(preset);
+    assertEquals("App Engine flexible environment with Java 8, Servlet 3.1", preset.getLabel());
+    assertThat(preset.getProjectFacets(), hasItem(JavaFacet.VERSION_1_8));
+    assertThat(preset.getProjectFacets(), hasItem(WebFacetUtils.WEB_31));
+    assertThat(preset.getProjectFacets(), hasItem(AppEngineFlexFacet.FACET_VERSION));
+  }
+}
---
+++ plugins/com.google.cloud.tools.eclipse.appengine.standard.java8.test/src/com/google/cloud/tools/eclipse/appengine/standard/java8/AppEngineStandardJava8PresetTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.eclipse.appengine.standard.java8;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import org.eclipse.jst.common.project.facet.core.JavaFacet;
+import org.eclipse.jst.j2ee.web.project.facet.WebFacetUtils;
+import org.eclipse.wst.common.project.facet.core.IPreset;
+import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
+import org.junit.Test;
+
+/**
+ * Test the App Engine standard environment for Java 8 preset.
+ */
+public class AppEngineStandardJava8PresetTest {
+  @Test
+  public void presetExists() {
+    IPreset preset = ProjectFacetsManager
+        .getPreset("com.google.cloud.tools.eclipse.appengine.standard.jre8.preset");
+    assertNotNull(preset);
+    assertEquals("App Engine standard environment with Java 8 (beta), Servlet 3.1",
+        preset.getLabel());
+    assertThat(preset.getProjectFacets(), hasItem(JavaFacet.VERSION_1_8));
+    assertThat(preset.getProjectFacets(), hasItem(WebFacetUtils.WEB_31));
+    assertThat(preset.getProjectFacets(),
+        hasItem(AppEngineStandardFacetChangeListener.APP_ENGINE_STANDARD_JRE8));
+  }
+}
