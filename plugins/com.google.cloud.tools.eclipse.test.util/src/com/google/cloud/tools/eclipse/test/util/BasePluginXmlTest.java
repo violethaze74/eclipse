95a0171eac283d23b54f58cd25ec1e7d17043d62
Chanseok Oh <chanseok@google.com>
2017-11-14 11:32:27 -0500

    Fix Guava import version (#2573)

--- plugins/com.google.cloud.tools.eclipse.test.util/src/com/google/cloud/tools/eclipse/test/util/BasePluginXmlTest.java
+++ plugins/com.google.cloud.tools.eclipse.test.util/src/com/google/cloud/tools/eclipse/test/util/BasePluginXmlTest.java
@@ -17,6 +17,7 @@
 package com.google.cloud.tools.eclipse.test.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import com.google.common.collect.Sets;
 import java.io.IOException;
@@ -26,6 +27,8 @@ import java.nio.file.Paths;
 import java.util.Set;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.RegistryFactory;
@@ -187,6 +190,35 @@ public abstract class BasePluginXmlTest {
       vendor = pluginProperties.get(vendor.substring(1));
     }
     assertEquals("Google Inc.", vendor);
+  }
+
+  @Test
+  public final void testGuavaImportVersions() throws IOException {
+    checkDependencyDirectives(
+        "Import-Package", "com.google.common.", "version=\"[20.0.0,21.0.0)\"");
+    checkDependencyDirectives(
+        "Require-Bundle", "com.google.guava", "bundle-version=\"[20.0.0,21.0.0)\"");
+  }
+
+  private void checkDependencyDirectives(
+      String attributeName, String prefixToCheck, String versionString) throws IOException {
+    String value = getManifestAttributes().getValue(attributeName);
+    if (value != null) {
+      String regexPrefix = prefixToCheck.replaceAll("\\.", "\\\\.");
+      Pattern pattern = Pattern.compile(regexPrefix + "[^;,]*");
+
+      Matcher matcher = pattern.matcher(value);
+      while (matcher.find()) {
+        int nextCharOffset = matcher.end();
+        if (nextCharOffset == value.length()) {
+          fail(attributeName + " directive not defined: " + matcher.group());
+        }
+        String stringAfterMatch = value.substring(nextCharOffset);
+        if (!stringAfterMatch.startsWith(";" + versionString)) {
+          fail(attributeName + " directive incorrect: " + matcher.group());
+        }
+      }
+    }
   }
 
   private static Attributes getManifestAttributes() throws IOException {
