bd88af077f282adf89f3aab55ce319b6d9343a42
Chanseok Oh <chanseok@google.com>
2017-12-12 10:07:09 -0500

    Let Kokoro pull Eclipse Oyxgen bundles from GCS (#2667)
    * Let Kokoro pull Eclipse from GCS mirror
    * Turn on echo again after gcloud.cmd

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -1,11 +1,15 @@
 @echo on
-cd github/google-cloud-eclipse
+cd github\google-cloud-eclipse
 
 set CLOUDSDK_CORE_DISABLE_USAGE_REPORTING=true
 call gcloud.cmd components update --quiet
+@echo on
 call gcloud.cmd components install app-engine-java --quiet
+@echo on
 
-mvn -B -N io.takari:maven:wrapper -Dmaven=3.5.0
-mvnw.cmd -B --fail-at-end -Ptravis -Declipse.target=oxygen verify
+mvn -B --settings kokoro\windows\m2-settings.xml ^
+    -N io.takari:maven:wrapper -Dmaven=3.5.0
+mvnw.cmd -B --settings kokoro\windows\m2-settings.xml ^
+         --fail-at-end -Ptravis -Declipse.target=oxygen verify
 
 exit /b %ERRORLEVEL%
