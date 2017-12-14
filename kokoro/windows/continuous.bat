134077704fde556b815af43f46427cf2f71b0b35
Chanseok Oh <chanseok@google.com>
2017-12-14 10:17:59 -0500

    Install dependency JARs that test projects require before starting Kokoro build  (#2683)
    * Pre-download dep artifacts of zipped test projects
    * Revert manual dep install trick
    * Clean when done
    * Remove Spring repos from pom.xml

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -1,6 +1,18 @@
 @echo on
 cd github\google-cloud-eclipse
 
+rem Pre-download all dependency JARs that test projects from the integration
+rem test require to avoid the concurrent download issue:
+rem https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/2284
+pushd plugins\com.google.cloud.tools.eclipse.integration.appengine\test-projects
+mkdir tmp-unzip-area
+cd tmp-unzip-area
+for %%i in (..\*.zip) do jar xf %%i
+for /f %%i in ('dir /b /s pom.xml') do mvn -B -q -f "%%i" package
+cd ..
+rmdir /s /q tmp-unzip-area
+popd
+
 set CLOUDSDK_CORE_DISABLE_USAGE_REPORTING=true
 call gcloud.cmd components update --quiet
 @echo on
