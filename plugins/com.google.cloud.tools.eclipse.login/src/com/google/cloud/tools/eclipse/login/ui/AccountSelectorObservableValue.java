1b952dc8ef6003ad8fef02e903335a5a7e199d6f
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2018-02-20 14:03:06 -0500

    fix warning (#2819)
    * ignore remote systems explorer junk
    
    * generics

--- plugins/com.google.cloud.tools.eclipse.login/src/com/google/cloud/tools/eclipse/login/ui/AccountSelectorObservableValue.java
+++ plugins/com.google.cloud.tools.eclipse.login/src/com/google/cloud/tools/eclipse/login/ui/AccountSelectorObservableValue.java
@@ -25,7 +25,7 @@ import org.eclipse.jface.databinding.swt.DisplayRealm;
  * changes (i.e., account selection changes in the combo box) can be tracked by value change
  * listeners.
  */
-public class AccountSelectorObservableValue extends AbstractObservableValue {
+public class AccountSelectorObservableValue extends AbstractObservableValue<String> {
 
   private String oldValue;
   private AccountSelector accountSelector;
@@ -50,12 +50,12 @@ public class AccountSelectorObservableValue extends AbstractObservableValue {
   }
 
   @Override
-  protected Object doGetValue() {
+  protected String doGetValue() {
     return accountSelector.getSelectedEmail();
   }
 
   @Override
-  protected void doSetValue(final Object value) {
-    accountSelector.selectAccount((String) value);
+  protected void doSetValue(final String value) {
+    accountSelector.selectAccount(value);
   }
 }
