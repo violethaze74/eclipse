e9ff9adfd1f8566b9996ce45788d303d2b8f2328
Chanseok Oh <chanseok@google.com>
2018-04-29 09:32:45 -0400

    Allow adding custom qualifier to branding version (#3035)
    * Allow adding custom qualifier to branding version
    * Enable adding version suffix through env var
    * Add suffix only to feature and branding versions

--- kokoro/ubuntu/new_release.sh
+++ kokoro/ubuntu/new_release.sh
@@ -15,6 +15,7 @@ gcloud components install app-engine-java --quiet
 echo "OAUTH_CLIENT_ID: ${OAUTH_CLIENT_ID}"
 echo "OAUTH_CLIENT_SECRET: ${OAUTH_CLIENT_SECRET}"
 echo "ANALYTICS_TRACKING_ID: ${ANALYTICS_TRACKING_ID}"
+echo "PRODUCT_VERSION_SUFFIX: "${PRODUCT_VERSION_SUFFIX}
 
 # Exit if undefined (zero-length).
 test -n "${OAUTH_CLIENT_ID}"
@@ -23,24 +24,17 @@ test -n "${ANALYTICS_TRACKING_ID}"
 
 cd git/google-cloud-eclipse
 
-# Drop the `.qualifier` in `metadata.product` and use the exact release
-# version. (For example, `version="1.3.0.qualifier"` to `version="1.3.0"`.)
-# version.
-#
-# https://github.com/GoogleCloudPlatform/google-cloud-eclipse/pull/2363#issuecomment-327844378
-# https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/2211
-readonly CT4E_DISPLAY_VERSION=$( \
-  xmlstarlet sel -t -v '/product/@version' gcp-repo/metadata.product )
-readonly NO_QUALIFIER_VERSION="${CT4E_DISPLAY_VERSION%.qualifier}"
-xmlstarlet ed --inplace -u '/product/@version' -v "${NO_QUALIFIER_VERSION}" \
-  gcp-repo/metadata.product
-
-# Need to unset `TMPDIR` for `xvfb-run` due to a bug:
-# https://bugs.launchpad.net/ubuntu/+source/xorg-server/+bug/972324
+# A few notes on the Maven command:
+#    - Need to unset `TMPDIR` for `xvfb-run` due to a bug:
+#      https://bugs.launchpad.net/ubuntu/+source/xorg-server/+bug/972324
+#    - Single-quotes are necessary for `-Dproduct.version.qualifier.suffix`,
+#      since it should be appended as a constant string in a date format.
 TMPDIR= xvfb-run \
-  mvn -V -B -Doauth.client.id="${OAUTH_CLIENT_ID}" \
-            -Doauth.client.secret="${OAUTH_CLIENT_SECRET}" \
-            -Dga.tracking.id="${ANALYTICS_TRACKING_ID}" \
+  mvn -V -B \
+      -Doauth.client.id="${OAUTH_CLIENT_ID}" \
+      -Doauth.client.secret="${OAUTH_CLIENT_SECRET}" \
+      -Dga.tracking.id="${ANALYTICS_TRACKING_ID}" \
+      ${PRODUCT_VERSION_SUFFIX:+-Dproduct.version.qualifier.suffix="'${PRODUCT_VERSION_SUFFIX}'"} \
     clean verify
 
 # Also export `metadata.product` and `metadata.p2.inf` to the second Kokoro job.
