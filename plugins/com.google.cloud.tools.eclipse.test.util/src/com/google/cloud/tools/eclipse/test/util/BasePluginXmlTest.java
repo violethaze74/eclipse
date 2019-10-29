12c7c6335ae02a9901e6de43c0e82cb38d95bc49
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2019-10-29 06:00:47 -0400

    update to appengine-plugins-core 0.8.1 (#3530)
    * update to appengine-plugins-core 0.8.1
    
    * update guava test

--- plugins/com.google.cloud.tools.eclipse.test.util/src/com/google/cloud/tools/eclipse/test/util/BasePluginXmlTest.java
+++ plugins/com.google.cloud.tools.eclipse.test.util/src/com/google/cloud/tools/eclipse/test/util/BasePluginXmlTest.java
@@ -277,9 +277,9 @@ public abstract class BasePluginXmlTest {
   @Test
   public final void testGuavaImportVersions() throws IOException {
     checkDependencyDirectives(
-        "Import-Package", "com.google.common.", "version=\"[27.0.0,28.0.0)\"");
+        "Import-Package", "com.google.common.", "version=\"[28.1.0,29.0.0)\"");
     checkDependencyDirectives(
-        "Require-Bundle", "com.google.guava", "bundle-version=\"[27.0.0,28.0.0)\"");
+        "Require-Bundle", "com.google.guava", "bundle-version=\"[28.1.0,29.0.0)\"");
   }
 
   @Test
